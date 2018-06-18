ebc6ef1814d7d5707a9c5856c8efc7d741d80ecf
Adetoyese Kola - Balogun <39604755+toye95@users.noreply.github.com>
2018-06-18 23:37:40 +0100

    Update note_routes.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -36,7 +36,7 @@ module.exports = function(app, db){
 
     });
 
-    app.get('/notes', (req, res) =>{
+    app.get('/*', (req, res) =>{
         res.sendFile(path.resolve(__dirname, 'client/build','index.html'));
         });
 
